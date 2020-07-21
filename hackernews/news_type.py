e0f206d50d70bbf6539d76d6e761c6d1bd6945af
Phil Kim <philipkim206@gmail.com>
2020-07-21 11:33:13 -0700

    consolidating get_stories function

--- hackernews/news_type.py
+++ hackernews/news_type.py
@@ -16,10 +16,71 @@ class getType:
     ITEM = 'item'
     ITEM_ID = []
 
+    # JOBS
+    async def get_job_stories(self):
+      async with aiohttp.ClientSession() as session:
+        async with session.get(
+          self.BASE_URL + self.JOBS + '.json?print=pretty') as top_ids:
+
+          data = await top_ids.json()
+
+          count = 0
+          while count < 100:
+            self.ITEM_ID.append(data[count])
+            count += 1
+
+      return self.ITEM_ID
+
+    # NEW_STORIES
+    async def get_new_stories(self):
+      async with aiohttp.ClientSession() as session:
+        async with session.get(
+          self.BASE_URL + self.NEW_STORIES + '.json?print=pretty') as top_ids:
+
+          data = await top_ids.json()
+
+          count = 0
+          while count < 100:
+            self.ITEM_ID.append(data[count])
+            count += 1
+
+      return self.ITEM_ID
+
+    # TOP_STORIES
     async def get_top_stories(self):
       async with aiohttp.ClientSession() as session:
         async with session.get(
           self.BASE_URL + self.TOP_STORIES + '.json?print=pretty') as top_ids:
+
+          data = await top_ids.json()
+
+          count = 0
+          while count < 100:
+            self.ITEM_ID.append(data[count])
+            count += 1
+
+      return self.ITEM_ID
+    
+    # BEST_STORIES
+    async def get_best_stories(self):
+      async with aiohttp.ClientSession() as session:
+        async with session.get(
+          self.BASE_URL + self.BEST_STORIES + '.json?print=pretty') as top_ids:
+
+          data = await top_ids.json()
+
+          count = 0
+          while count < 100:
+            self.ITEM_ID.append(data[count])
+            count += 1
+
+      return self.ITEM_ID
+    
+    # ASK
+    async def get_ask_stories(self):
+      async with aiohttp.ClientSession() as session:
+        async with session.get(
+          self.BASE_URL + self.ASK + '.json?print=pretty') as top_ids:
 
           data = await top_ids.json()
 
